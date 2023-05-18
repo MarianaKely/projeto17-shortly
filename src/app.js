78febcf3bfad94b964aa68891ad0b0a24f77fce3
MarianaKely <marianakely.r@gmail.com>
2023-05-18 19:40:59 -0300

    registers router implemented

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import registerRouters from "./routers/registerRouter.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(registerRouters);
 
 
 const PORT = process.env.PORT || 5000;
