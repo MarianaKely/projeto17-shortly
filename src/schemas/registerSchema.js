8b3b0e3ae52ab1a35db319722e0b5f4a920e8a01
MarianaKely <marianakely.r@gmail.com>
2023-05-18 19:40:11 -0300

    signup schema implemented

---
+++ src/schemas/registerSchema.js
@@ -0,0 +1,11 @@
+
+import Joi from "joi";
+
+export const registerSchema = Joi.object({
+
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    confirmPassword: Joi.string().required().valid(Joi.ref("password")),
+
+  });
