c3fb218ed654091b9a26f762df28289308b5e5c9
MarianaKely <marianakely.r@gmail.com>
2023-05-18 21:18:01 -0300

    signup schema update

--- src/schemas/registerSchema.js
+++ src/schemas/registerSchema.js
@@ -1,11 +1,38 @@
 
 import Joi from "joi";
 
-export const registerSchema = Joi.object({
+const shortSignUpSchema = Joi.object({
 
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-    confirmPassword: Joi.string().required().valid(Joi.ref("password")),
+  name: Joi.string().required().messages({
 
-  });
+    "any.required": 'Obrigatório preeencher "name"',
+    "string.empty": '"name" não pode estar vazio.',
+
+  }),
+
+  email: Joi.string().email().required().messages({
+
+    "any.required": 'Obrigatório preencher "email"',
+    "string.empty": '"email" não pode estar vazio.',
+    "string.email": 'Opa! "email" inválido!',
+
+  }),
+
+  password: Joi.string().required().messages({
+
+    "any.required": 'Obrigatório preencher "password".',
+    "string.empty": '"password" pode estar vazio.',
+
+  }),
+
+  confirmPassword: Joi.string().valid(Joi.ref("password")).required().messages({
+
+    "any.required": 'Obrigatório preencher "confirmPassword".',
+    "string.empty": '"confirmPassword" não pode estar vazio.',
+    "any.only": 'Opa! "confirmPassword" não está igual a "password"!.',
+    
+  }),
+
+});
+
+export default shortSignUpSchema;
