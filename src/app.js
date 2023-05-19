1bce0c937d10447aea4108cc1502b3b9f4fd08f1
MarianaKely <marianakely.r@gmail.com>
2023-05-18 22:52:14 -0300

    urls router implemented

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import registerRouter from "./routers/registerRouter.js";
+import urlsRouter from "./routers/urlRouters.js";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(registerRouter);
+app.use(urlsRouter);
 
 
 const PORT = process.env.PORT || 5000;
